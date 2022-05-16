c16edbdefd8cb800d7f1e028ff17e0fbeb937341
Jaroslavna Kuptsova <yaroslavna121@yandex.ru>
2022-05-16 12:08:00 +0300

    AddPlacePopup and ConfirmDeletePopup

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,100 @@ import React from 'react'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
-import PopupWithForm from './PopupWithForm'
 import ImagePopup from './ImagePopup'
 import EditProfilePopup from './EditProfilePopup'
 import EditAvatarPopup from './EditAvatarPopup'
+import AddPlacePopup from './AddPlacePopup'
+import ConfirmDeletePopup from './ConfirmDeletePopup'
 import api from '../utils/api.js'
 import CurrentUserContext from '../contexts/CurrentUserContext'
 
 function App() {
+  // component states
   const [currentUser, setCurrentUser] = React.useState({
     _id: null,
     name: '',
     description: '',
     avatar: null,
   })
+  const [cards, setCards] = React.useState([])
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(
+    false,
+  )
+  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false)
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false)
+  const [isConfirmDeletePopupOpen, setConfirmDeletePopupOpen] = React.useState(
+    false,
+  )
+  const [selectedCard, setSelectedCard] = React.useState(null)
+  const [deletingCard, setDeletingCard] = React.useState(null)
+
+  // handling popups
+  const closeAllPopups = () => {
+    setEditProfilePopupOpen(false)
+    setEditAvatarPopupOpen(false)
+    setAddPlacePopupOpen(false)
+    setConfirmDeletePopupOpen(false)
+    setSelectedCard(null)
+  }
+
+  // handling user profile
+  const handleEditProfileClick = () => setEditProfilePopupOpen(true)
+  const handleUpdateUser = (user) => {
+    closeAllPopups()
+    api
+      .setUserInfo(user)
+      .then((user) => setCurrentUser(user))
+      .catch((error) => {
+        console.log('Could not update user info:', error)
+      })
+  }
+
+  // handling avatar
+  const handleEditAvatarClick = () => setEditAvatarPopupOpen(true)
+  const handleUpdateAvatar = (data) => {
+    closeAllPopups()
+    api
+      .setUserAvatar(data)
+      .then((user) => setCurrentUser(user))
+      .catch((error) => {
+        console.log('Could not update user avatar:', error)
+      })
+  }
+
+  // handling cards
+  const handleAddPlaceClick = () => setAddPlacePopupOpen(true)
+  const handleCardClick = (card) => setSelectedCard(card)
+  const handleImagePopupClose = () => setSelectedCard(null)
+  function handleCardLike(card) {
+    ;(card.liked
+      ? api.unlikeCard(card._id, currentUser._id)
+      : api.likeCard(card._id, currentUser._id)
+    ).then((newCard) => {
+      setCards((cards) => cards.map((c) => (c._id === card._id ? newCard : c)))
+    })
+  }
+  function handleCardDelete(card) {
+    setDeletingCard(card)
+    setConfirmDeletePopupOpen(true)
+  }
+  function handleConfirmDeleteCard() {
+    closeAllPopups()
+    api.deleteCard(deletingCard._id).then(() => {
+      setCards((cards) => cards.filter((c) => c._id !== deletingCard._id))
+    })
+  }
+  function handleAddPlace(card) {
+    closeAllPopups()
+    api
+      .addCard(card)
+      .then((card) => {
+        setCards([card, ...cards])
+      })
+      .catch((error) => {
+        console.log('Could not save card:', error)
+      })
+  }
 
   // fetch currentUser on component mount
   React.useEffect(() => {
@@ -34,42 +114,17 @@ function App() {
       })
   }, [])
 
-  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(
-    false,
-  )
-  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false)
-  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false)
-  const [selectedCard, setSelectedCard] = React.useState(null)
-
-  const handleEditProfileClick = () => setEditProfilePopupOpen(true)
-  const handleEditAvatarClick = () => setEditAvatarPopupOpen(true)
-  const handleAddPlaceClick = () => setAddPlacePopupOpen(true)
-  const closeAllPopups = () => {
-    setEditProfilePopupOpen(false)
-    setEditAvatarPopupOpen(false)
-    setAddPlacePopupOpen(false)
-    setSelectedCard(null)
-  }
-  const handleCardClick = (card) => setSelectedCard(card)
-  const handleImagePopupClose = () => setSelectedCard(null)
-  const handleUpdateUser = (user) => {
-    closeAllPopups()
+  // fetch cards on current user update
+  React.useEffect(() => {
     api
-      .setUserInfo(user)
-      .then((user) => setCurrentUser(user))
-      .catch((error) => {
-        console.log('Could not update user info:', error)
+      .getInitialCards(currentUser._id)
+      .then((cards) => {
+        setCards(cards)
       })
-  }
-  const handleUpdateAvatar = (data) => {
-    closeAllPopups()
-    api
-      .setUserAvatar(data)
-      .then((user) => setCurrentUser(user))
       .catch((error) => {
-        console.log('Could not update user avatar:', error)
+        console.log('Could not fetch cards:', error)
       })
-  }
+  }, [currentUser._id])
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
@@ -77,6 +132,9 @@ function App() {
         <Header />
 
         <Main
+          cards={cards}
+          onCardLike={handleCardLike}
+          onCardDelete={handleCardDelete}
           onEditProfile={handleEditProfileClick}
           onEditAvatar={handleEditAvatarClick}
           onAddPlace={handleAddPlaceClick}
@@ -97,41 +155,16 @@ function App() {
           onUpdateAvatar={handleUpdateAvatar}
         />
 
-        <PopupWithForm
-          name="card-add"
-          title="Новое место"
-          saveButtonName="Создать"
+        <AddPlacePopup
           isOpen={isAddPlacePopupOpen}
           onClose={closeAllPopups}
-        >
-          <label className="popup__form-field">
-            <input
-              className="popup__input popup__input_type_name"
-              name="card_name"
-              type="text"
-              placeholder="Название"
-              minLength="2"
-              maxLength="30"
-              required
-            />
-            <span className="popup__input-error popup__input-error_type_name"></span>
-          </label>
-          <label className="popup__form-field">
-            <input
-              className="popup__input popup__input_type_info"
-              name="link"
-              type="url"
-              placeholder="Ссылка на картинку"
-              required
-            />
-            <span className="popup__input-error popup__input-error_type_info"></span>
-          </label>
-        </PopupWithForm>
+          onAddPlace={handleAddPlace}
+        />
 
-        <PopupWithForm
-          name="confirm-trash"
-          title="Вы уверены?"
-          saveButtonName="Да"
+        <ConfirmDeletePopup
+          isOpen={isConfirmDeletePopupOpen}
+          onClose={closeAllPopups}
+          onConfirm={handleConfirmDeleteCard}
         />
 
         <ImagePopup card={selectedCard} onClose={handleImagePopupClose} />
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,10 @@
 import React from 'react'
 import Card from './Card.js'
 import pencil from '../images/pencil.svg'
-import api from '../utils/api.js'
 import CurrentUserContext from '../contexts/CurrentUserContext'
 
 function Main(props) {
   const currentUser = React.useContext(CurrentUserContext)
-  const [cards, setCards] = React.useState([])
-
-  React.useEffect(() => {
-    api
-      .getInitialCards(currentUser._id)
-      .then((cards) => {
-        setCards(cards)
-      })
-      .catch((error) => {
-        console.log('Could not fetch cards:', error)
-      })
-  }, [currentUser._id])
-
-  function handleCardLike(card) {
-    ;(card.liked
-      ? api.unlikeCard(card._id, currentUser._id)
-      : api.likeCard(card._id, currentUser._id)
-    ).then((newCard) => {
-      setCards((cards) => cards.map((c) => (c._id === card._id ? newCard : c)))
-    })
-  }
-
-  function handleCardDelete(card) {
-    api.deleteCard(card._id).then(() => {
-      setCards((cards) => cards.filter((c) => c._id !== card._id))
-    })
-  }
 
   const profileEditClassName =
     'profile__edit-button ' +
@@ -74,13 +46,13 @@ function Main(props) {
 
       <section className="elements">
         <div className="elements__content">
-          {cards.map((card) => (
+          {props.cards.map((card) => (
             <Card
               key={card._id}
               card={card}
               onCardClick={props.onCardClick}
-              onCardLike={handleCardLike}
-              onCardDelete={handleCardDelete}
+              onCardLike={props.onCardLike}
+              onCardDelete={props.onCardDelete}
             />
           ))}
         </div>
